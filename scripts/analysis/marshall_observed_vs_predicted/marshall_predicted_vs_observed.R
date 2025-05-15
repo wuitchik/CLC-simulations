a003b914f8db47c4b57c922a81328383926ce338
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2025-05-15 11:08:50 -0700

    coastal map

--- scripts/analysis/marshall_observed_vs_predicted/marshall_predicted_vs_observed.R
+++ scripts/analysis/marshall_observed_vs_predicted/marshall_predicted_vs_observed.R
@@ -3,7 +3,7 @@ library(viridis)
 library(patchwork)
 
 # Load and clean data
-df <- read_csv("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/data/processed/eta_ground_YESseason_summary.csv") %>%
+df <- read_csv("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/data/processed/eta_ground_YESseason_summary_negCOV.csv") %>%
   filter(if_all(everything(), is.finite)) %>%
   filter(!if_any(everything(), ~ . < 0)) %>%
   filter(eta < 0.32) %>%
@@ -49,5 +49,5 @@ p2 <- ggplot(df, aes(x = avg_predLagAdults, y = avg_obsLagAdults, fill = eta)) +
 
 # Combine with patchwork and add labels A) and B)
 combined_plot <- p1 + p2 + plot_annotation(tag_levels = "A")
-ggsave("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/figs/ground_truth/season_pred_vs_obser.png", combined_plot, width = 10, height = 5)
-ggsave("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/figs/ground_truth/season_pred_vs_obser.pdf", combined_plot, width = 10, height = 5)
+ggsave("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/figs/ground_truth/season_negCOV_pred_vs_obser.png", combined_plot, width = 10, height = 5)
+ggsave("/Users/danielwuitchik/Documents/Experiments/Crepidula_tufts/Github/CLC-simulations/figs/ground_truth/season_negCOV_pred_vs_obser.pdf", combined_plot, width = 10, height = 5)
